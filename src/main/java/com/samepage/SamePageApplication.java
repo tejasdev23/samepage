19b223e47bc3fd4acec305dcddee268b14b7cbfc
Tejas Patel <tejaspatel@Tejass-MacBook-Pro.local>
2026-03-03 21:31:10 -0500

    feat: initial green build with java 21 and ci/cd

---
+++ src/main/java/com/samepage/SamePageApplication.java
@@ -0,0 +1,12 @@
+package com.samepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamePageApplication.class, args);
+	}
+}
